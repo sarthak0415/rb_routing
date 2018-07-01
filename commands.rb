f61462bfede15c9f083d73c36c9c7f2d76029663
sarthak <sarthak0415@github.com>
2018-07-01 18:21:58 +0530

    remove commands already present in readme

--- commands.rb
+++
@@ -1,4 +0,0 @@
-require 'pp'	
-require 'rb_routing'
-router = RbRouting::Router::Dijkstra.new  :host => 'localhost', :database => 'routing', :user => 'sarthak', :edge_table => :ways ,:id => :gid, :cost => :cost, :reverse_cost => :reverse_cost 
-router.run :source => 3, :target => 4
